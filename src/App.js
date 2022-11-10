588e518db5487ca83bf106b152f948b6b33e59c2
TanmayJain872 <53950752+TanmayJain872@users.noreply.github.com>
2022-11-10 15:16:21 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 /* jshint esversion: 6 */
-import React, { useState } from 'react';
+import React from 'react';
 import { Routes, Route } from 'react-router-dom';
 import './App.css';
 import { theme, ThemeProvider } from './components/CustomAppTheme';
@@ -7,24 +7,14 @@ import LoginForm from './components/LoginForm';
 import RegistrationForm from './components/RegistrationForm';
 import Home from './components/Home';
 
-export const CustomerIDsContext = React.createContext('');
-
 function App() {
-  let [customerID, setCustomerID] = useState('');
+  
   return (
     <ThemeProvider theme={theme}>
       <Routes>
-        <Route path='/' element={
-          <CustomerIDsContext.Provider value={[customerID, setCustomerID]}>
-            <LoginForm />
-          </CustomerIDsContext.Provider>
-        } />
-        <Route path='/home' element={
-          <CustomerIDsContext.Provider value={[customerID, setCustomerID]}>
-            <Home />
-          </CustomerIDsContext.Provider>
-        } />
-        <Route path='/signup' element={<RegistrationForm />} />
+        <Route path='/' element={ <LoginForm /> } />
+        <Route path='/home' element={ <Home /> } />
+        <Route path='/signup' element={ <RegistrationForm /> } />
       </Routes>
     </ThemeProvider>
   );
